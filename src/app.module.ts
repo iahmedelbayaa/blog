fd26b1c501a1aaebb8ddeae509b2ddbf22fbe062
iahmedelbayaa <elbayaaahmed845@gmail.com>
2024-01-23 01:41:16 +0200

    Docs docker compose & pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,15 +8,16 @@ import {TypeOrmModule} from '@nestjs/typeorm'
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: parseInt(process.env.POSTGRES_PORT,10),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      autoLoadEntities: true,
-      synchronize: true,
-    })
+  "type": "postgres",
+  "host": "localhost",
+  "port": 5432,
+  "username": process.env.POSTGRES_USER,
+  "password": process.env.POSTGRES_PASSWORD,
+  "database": process.env.POSTGRES_DB,
+  "synchronize": true,
+  "logging": true
+})
+
   ],
   controllers: [AppController],
   providers: [AppService],
