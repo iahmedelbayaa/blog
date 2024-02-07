e5c6d88fa988c5908680fb353b0b0104c72debe9
iahmedelbayaa <elbayaaahmed845@gmail.com>
2024-02-07 23:59:59 +0200

    ref role & jwt

--- src/auth/guards/jwt.strategy.ts
+++ src/auth/guards/jwt.strategy.ts
@@ -1,22 +1,20 @@
-import { Injectable } from "@nestjs/common";
-import { ExtractJwt, Strategy } from "passport-jwt";
-import { PassportStrategy } from '@nestjs/passport'
-import { ConfigService } from "@nestjs/config";
 
-
-
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(private configService : ConfigService) {
-        super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            ignoreExpiration: true,
-            secretOrKey: configService.get('JWT_SECRET')
-        })
-    }
+  constructor(private configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
 
-    async validate(payload: any) {
-        return { 'user' : payload.user }
-    }
+  async validate(payload: any) {
+    return { ...payload.user };
+  }
 }
--- src/auth/guards/role.guard.ts
+++ src/auth/guards/role.guard.ts
@@ -1,4 +1,4 @@
-import { CanActivate, ExecutionContext, Inject, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable, map } from 'rxjs';
 import { User } from 'src/user/models/user.interface';
