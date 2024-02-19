d67ee659a3f00920f21073582661cd7527826a79
iahmedelbayaa <elbayaaahmed845@gmail.com>
2024-02-19 21:11:22 +0200

    Docs test with jest

--- src/user/controller/user.controller.spec.ts
+++ src/user/controller/user.controller.spec.ts
@@ -1,13 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserService } from '../service/user.service';
 
 describe('UserController', () => {
   let controller: UserController;
 
+  const mockUsersService = {
+    create: jest.fn(dto => {
+      return {
+        id: Date.now(),
+        ...dto,
+      };
+    }),
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-    }).compile();
+      providers: [UserService],
+    })
+    .overrideProvider(UserService)
+    .useValue(mockUsersService)
+    .compile();
 
     controller = module.get<UserController>(UserController);
   });
@@ -15,4 +29,11 @@ describe('UserController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
-});
+
+  it('should create a user', () => {
+    expect(controller.create({name: 'John Doe'})).toEqual({
+      id: expect.any(Number),
+      name: 'John Doe',
+    });
+  })
+})
